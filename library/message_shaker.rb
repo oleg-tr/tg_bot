688caf3dafe55f3a4bc68a78fb55ab187e71e6c0
olegalbegov <oleg.albegov@onetwotrip.com>
2022-05-18 22:25:54 +0300

    securty

--- library/message_shaker.rb
+++ library/message_shaker.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module TelegramOrientedInfo
-    API_KEY = '5328212661:AAH33W3meCZI-G7DHt5cQ0lKX1Ot0L2Wq0s'
+    API_KEY = '5328212661:AAF3S-W4FkOz8uuLM_T9GvASj-21hceUOTw'
 end
